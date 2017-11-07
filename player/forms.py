105513cd66807b06cec8aa2dcb54d6bd2a009983
otsab <you@example.com>
2017-11-07 09:37:27 +0545

    new

--- player/forms.py
+++ player/forms.py
@@ -3,7 +3,9 @@ from django import forms
 from .models import Song, Album
 
 class UserForm(forms.ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput)
+	username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+	email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 	class Meta:
 		model = User
